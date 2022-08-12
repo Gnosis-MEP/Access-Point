a386757746178329a44c150e01f8bf1f6e19a051
tarzaa <tarek.zaarour@insight-centre.org>
2022-08-12 10:45:07 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,61 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+
+hostName = "localhost"
+serverPort = 8080
+
+class HTTPHandler(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+        self.end_headers()
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
+
+    def do_OPTIONS(self):
+        self._set_headers()
+
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.get('content-length'))
+        message = json.loads(self.rfile.read(length))
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(str.encode(json.dumps(message)))
+
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), HTTPHandler)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
