719b3b0b82fb9735689587db39ea9c3e66a9c66b
tarzaa <tarek.zaarour@insight-centre.org>
2022-11-22 12:02:51 +0000

    added rws server

--- access_point/service.py
+++ access_point/service.py
@@ -4,14 +4,15 @@ from event_service_utils.logging.decorators import timer_logger
 from event_service_utils.services.event_driven import BaseEventDrivenCMDService
 from event_service_utils.tracing.jaeger import init_tracer
 
+import time
+
 
 class AccessPoint(BaseEventDrivenCMDService):
     def __init__(self,
                  service_stream_key, service_cmd_key_list,
                  pub_event_list, service_details,
+                 rws_server,
                  stream_factory,
-                 query_received_stream_key,
-                 publisher_created_stream_key,
                  logging_level,
                  tracer_configs):
         tracer = init_tracer(self.__class__.__name__, **tracer_configs)
@@ -28,19 +29,14 @@ class AccessPoint(BaseEventDrivenCMDService):
         self.cmd_validation_fields = ['id']
         self.data_validation_fields = ['id']
 
-        self.query_received_stream_key = query_received_stream_key
-        self.query_received_stream = self.stream_factory.create(key=query_received_stream_key, stype='streamOnly')
-
-        self.publisher_created_stream_key = publisher_created_stream_key
-        self.publisher_created_stream = self.stream_factory.create(key=publisher_created_stream_key, stype='streamOnly')
+        self.rws_server = rws_server
+        self.rws_server.access_point = self
 
     def send_event_to_publisher_created(self, event_data):
-        self.logger.debug(f'Sending event to publisher_created: {event_data}')
-        self.write_event_with_trace(event_data, self.publisher_created_stream)
+        self.publish_event_type_to_stream('PublisherCreated', event_data)
 
     def send_event_to_query_received(self, event_data):
-        self.logger.debug(f'Sending event to query_received: {event_data}')
-        self.write_event_with_trace(event_data, self.query_received_stream)
+        self.publish_event_type_to_stream('QueryReceived', event_data)
 
     @timer_logger
     def process_data_event(self, event_data, json_msg):
@@ -64,6 +60,7 @@ class AccessPoint(BaseEventDrivenCMDService):
         self.logger.info(f'Service name: {self.name}')
         # function for simple logging of python dictionary  
         # self._log_dict('Some Dictionary', self.some_dict)
+        self._log_dict('Consumer groups', self.service_cmd_cg_stream_map)
 
     def run(self):
         super(AccessPoint, self).run()
@@ -72,5 +69,6 @@ class AccessPoint(BaseEventDrivenCMDService):
         self.data_thread = threading.Thread(target=self.run_forever, args=(self.process_data,))
         self.cmd_thread.start()
         self.data_thread.start()
+        self.rws_server.serve_forever()
         self.cmd_thread.join()
         self.data_thread.join()
